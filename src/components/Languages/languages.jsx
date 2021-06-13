5d1e4042c0ddf6f3f38bb2ca5433dfe991338a52
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-13 16:49:28 +0300

    style languages

--- src/components/Languages/languages.jsx
+++ src/components/Languages/languages.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  list: {
+    margitLeft: 20,
+    fontSize: 14,
+    lineHeight: 1.6,
+    color: '#8ea143'
+  },
+  label: {
+    color: '#fcfce8',
+  }
+
+})
 
 const Languages = ({ label }) => {
+
+  const classes = useStyles()
+
   return (
-    <li>
-      <p>{label}</p>
+    <li className={classes.list}>
+      <span className={classes.label}>{label}</span>
     </li>
   )
 };
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createUseStyles } from 'react-jss';
 import PropTypes from 'prop-types';
 
-
 import TechSkills from '../TechSkills';
 import SoftSkills from '../SoftSkills';
 import Contacts from '../Contacts';
@@ -11,8 +10,8 @@ import Languages from '../Languages';
 const useStyles = createUseStyles({
   sidebar: {
     minWidth: 300,
-    padding: 40 ,
-    background: '#525151'
+    padding: 40,
+    background: '#525151',
   },
   section: {
     marginTop: 50,
@@ -21,67 +20,57 @@ const useStyles = createUseStyles({
     fontSize: 18,
     lineHeight: 1.6,
     color: '#fff',
-  }
-
-})
+  },
+});
 
 const SideBar = ({ contacts, techSkills, softSkills, languages }) => {
-
   const classes = useStyles();
 
   return (
-      <aside className={classes.sidebar}>
-        {/* <img src={photo} alt='Vadim Vatrya' className={style.photo}/> */}
-        <div className={classes.section}>
-          <h3 className={classes.title}>Tech Skills</h3>
-          <ul>
-            {techSkills.map(({ id, label }) => (
-              <TechSkills 
-              key={id} 
-              label={label} 
-              />
-            ))}
-          </ul>
-        </div>
+    <aside className={classes.sidebar}>
+      {/* <img src={photo} alt='Vadim Vatrya' className={style.photo}/> */}
+      <div className={classes.section}>
+        <h3 className={classes.title}>Tech Skills</h3>
+        <ul>
+          {techSkills.map(({ id, label }) => (
+            <TechSkills key={id} label={label} />
+          ))}
+        </ul>
+      </div>
 
-        <div className={classes.section}>
-          <h3 className={classes.title}>Soft Skills</h3>
-          <ul>
-            {softSkills.map(({ id, label }) => (
-              <SoftSkills
-               key={id}
-               label={label}/>
-            ))}
-          </ul>
-        </div>
+      <div className={classes.section}>
+        <h3 className={classes.title}>Soft Skills</h3>
+        <ul>
+          {softSkills.map(({ id, label }) => (
+            <SoftSkills key={id} label={label} />
+          ))}
+        </ul>
+      </div>
 
-        <div className={classes.section}>
-          <h3 className={classes.title}>Contacts</h3>
-          <ul>
-            {contacts.map(({ id, item, link, name, icon }) => (
-              <Contacts
-                key={id}
-                item={item}
-                link={link}
-                name={name}
-                icon={icon}
-              />
-            ))}
-          </ul>
-        </div>
+      <div className={classes.section}>
+        <h3 className={classes.title}>Languages</h3>
+        <ul>
+          {languages.map(({ id, label }) => (
+            <Languages key={id} label={label} />
+          ))}
+        </ul>
+      </div>
 
-        <div className={classes.section}>
-          <h3 className={classes.title}>Languages</h3>
-          <ul>
-            {languages.map(({ id, label }) => (
-              <Languages 
-               key={id} 
-               label={label}/>
-            ))}
-          </ul>
-        </div>
-      </aside>
-  
+      <div className={classes.section}>
+        <h3 className={classes.title}>Contacts</h3>
+        <ul>
+          {contacts.map(({ id, item, link, name, icon }) => (
+            <Contacts
+              key={id}
+              item={item}
+              link={link}
+              name={name}
+              icon={icon}
+            />
+          ))}
+        </ul>
+      </div>
+    </aside>
   );
 };
 
