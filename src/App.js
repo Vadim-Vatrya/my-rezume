88efa0e6a745d8ec72248042853bb4b7893ef206
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-13 23:00:58 +0300

    fix1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import techSkills from './components/json/techSkills.json';
 import softSkills from './components/json/softSkills.json';
 import languages from './components/json/languages.json';
 import teamWork from './components/json/teamWork.json';
+// import teamWork from './components/json/teamWork1.json';
 import personalWork from './components/json/personalWork.json';
 import education from './components/json/education.json';
 import workExperience from './components/json/workExperience.json';
